7b85e81d06b9c77b1fae2b7ee61c5a1efd563b93
Neli Shahapuni <neli.shahapuni@mac046.local>
2023-10-29 10:05:35 +0200

    Create Project
    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # share-extension
-This document outlines the requirements for an exercise for iOS share extensions and code/data sharing between the app and the extension.
+This repo contains an iOS share extensions for code/data sharing between the app and the extension.
---
+++ ShareExtension/ShareExtension/ShareExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareExtensionApp.swift
+//  ShareExtension
+//
+//  Created by Neli Shahapuni on 29.10.23.
+//
+
+import SwiftUI
+
+@main
+struct ShareExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
